980fa839918db05d30cb9fde2233dcda608a194d
Leo Irakliotis <lgreco@gmail.com>
2025-06-11 22:00:12 -0500

    Add files via upload

---
+++ TrainLine_Implementation.java
@@ -0,0 +1,10 @@
+public class TrainLine_Implementation {
+    public static void main(String[] args) {
+        TrainLine redLineSB = new TrainLine("Red Line Southbound");
+        redLineSB.add("Howard");
+        redLineSB.add("Jarvis");
+        redLineSB.add("Morse");
+        redLineSB.add("Loyola");
+        System.out.println(redLineSB);
+    }
+}
